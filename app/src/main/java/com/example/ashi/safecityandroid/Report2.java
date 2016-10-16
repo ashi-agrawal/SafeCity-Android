147f58a97d477c6b390b2ea1f2440727a2da90ab
carolineh101 <cho19@stanford.edu>
2016-10-15 20:00:24 -0700

    Creates home screen and adds reporting and map screens

---
+++ app/src/main/java/com/example/ashi/safecityandroid/Report2.java
@@ -0,0 +1,16 @@
+package com.example.ashi.safecityandroid;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Report2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        setContentView(R.layout.activity_report2);
+    }
+}
