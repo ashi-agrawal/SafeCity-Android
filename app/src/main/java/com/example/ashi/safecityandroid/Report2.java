fa5531ae5779bbebedde48ecaf43b6a79017a062
carolineh101 <cho19@stanford.edu>
2016-10-16 19:53:54 -0700

    Creates Report Welcome and Report Step 2 screens

--- app/src/main/java/com/example/ashi/safecityandroid/Report2.java
+++ app/src/main/java/com/example/ashi/safecityandroid/Report2.java
@@ -1,10 +1,15 @@
 package com.example.ashi.safecityandroid;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Report2 extends AppCompatActivity {
+
+    Button mToStep3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +17,17 @@ public class Report2 extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
         setContentView(R.layout.activity_report2);
+
+        mToStep3 = (Button) findViewById(R.id.toStep3);
+
+        View.OnClickListener toStep3Listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Report2.this, Report3.class);
+                startActivity(intent);
+            }
+        };
+
+        mToStep3.setOnClickListener(toStep3Listener);
     }
 }
--- app/src/main/java/com/example/ashi/safecityandroid/ReportWelcome.java
+++ app/src/main/java/com/example/ashi/safecityandroid/ReportWelcome.java
@@ -1,10 +1,15 @@
 package com.example.ashi.safecityandroid;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ReportWelcome extends AppCompatActivity {
+
+    Button mStartReport;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +17,17 @@ public class ReportWelcome extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
         setContentView(R.layout.activity_report_welcome);
+
+        mStartReport = (Button) findViewById(R.id.startReport);
+
+        View.OnClickListener toStep1Listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ReportWelcome.this, Report1.class);
+                startActivity(intent);
+            }
+        };
+
+        mStartReport.setOnClickListener(toStep1Listener);
     }
 }
