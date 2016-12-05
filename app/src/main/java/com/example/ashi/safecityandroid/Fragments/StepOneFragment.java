6bfb060cb75817aeb16e4ff8e661f7dfacec98e2
bronwyn6 <bronwyn.early@gmail.com>
2016-12-05 13:22:25 -0800

    Scrolling It's lit fam

--- app/src/main/java/com/example/ashi/safecityandroid/Fragments/StepOneFragment.java
+++ app/src/main/java/com/example/ashi/safecityandroid/Fragments/StepOneFragment.java
@@ -50,22 +50,22 @@ public class StepOneFragment extends Fragment {
         //        getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
 
 
-        SupportPlaceAutocompleteFragment autocompleteFragment  = (SupportPlaceAutocompleteFragment)
-                getActivity().getSupportFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
-
-        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
-            @Override
-            public void onPlaceSelected(Place place) {
-                // TODO: Get info about the selected place.
-                Log.i(TAG, "Place: " + place.getName());
-            }
-
-            @Override
-            public void onError(Status status) {
-                // TODO: Handle the error.
-                Log.i(TAG, "An error occurred: " + status);
-            }
-        });
+//        SupportPlaceAutocompleteFragment autocompleteFragment  = (SupportPlaceAutocompleteFragment)
+//                getActivity().getSupportFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+//
+//        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+//            @Override
+//            public void onPlaceSelected(Place place) {
+//                // TODO: Get info about the selected place.
+//                Log.i(TAG, "Place: " + place.getName());
+//            }
+//
+//            @Override
+//            public void onError(Status status) {
+//                // TODO: Handle the error.
+//                Log.i(TAG, "An error occurred: " + status);
+//            }
+//        });
     }
 
     @Nullable
